00866d9bf7480f406f367d35cd098972ab9ef2ec
THEEamaizing <ambimwaluseke@gmail.com>
2025-04-06 22:49:16 +0300

    Trying user input

--- hello.kt
+++ hello.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello from Kotlin in VS Code!")
+    print("Enter your name: ")
+    val name = readLine()
+
+    print("Enter your age: ")
+    val age = readLine()?.toIntOrNull()
+
+    if (age != null) {
+        println("Hello, $name! You are $age years old.")
+    } else {
+        println("Oops! That doesn't look like a valid age.")
+    }
 }
